c77e843fff4180986a1d695398a211a05e7657e0
prodonik <rruzibayevich@gmail.com>
2024-06-03 02:58:22 +0500

    message

--- internal/usecase/repo/animal.go
+++ internal/usecase/repo/animal.go
@@ -5,6 +5,7 @@ import (
 	"Farmish/pkg/postgres"
 	"context"
 
+	"github.com/Masterminds/squirrel"
 	"github.com/pkg/errors"
 )
 
@@ -46,21 +47,89 @@ func (r *AnimalRepo) CreateAnimal(ctx context.Context, request *entity.Animal) (
 }
 
 func (r *AnimalRepo) GetAnimalByID(ctx context.Context, request string) (*entity.Animal, error) {
-	//implement here
-	return nil, errors.New("unimplemented method - GetAnimal")
+	var (
+		response entity.Animal
+	)
+
+	sql, _, err := r.Builder.Select("id", "name", "weight", "is_hungry").
+		From("animals").
+		Where(squirrel.Eq{"id": request}).
+		ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	row := r.Pool.QueryRow(ctx, sql)
+
+	if err := row.Scan(&response.ID, &response.Name, &response.Weight); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }
 
 func (r *AnimalRepo) UpdateAnimal(ctx context.Context, request *entity.Animal) (*entity.Animal, error) {
-	//implement here
-	return nil, errors.New("unimplemented method - UpdateAnimal")
+	var (
+		response entity.Animal
+	)
+
+	data := map[string]interface{}{
+		"id":        request.ID,
+		"name":      request.Name,
+		"weight":    request.Weight,
+		"is_hungry": request.IsHungry,
+	}
+
+	sql, _, err := r.Builder.Update("animals").
+		SetMap(data).
+		Where(squirrel.Eq{"id": request.ID}).
+		ToSql()
+
+	if err != nil {
+		return nil, err
+	}
+
+	row := r.Pool.QueryRow(ctx, sql)
+
+	if err := row.Scan(&response.ID, &response.Name, &response.Weight); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }
 
 func (r *AnimalRepo) DeleteAnimal(ctx context.Context, id string) (*entity.Animal, error) {
-	//implement here
-	return nil, errors.New("unimplemented method - DeleteAnimal")
+	var (
+		response entity.Animal
+	)
+
+	sql, _, err := r.Builder.Delete("animals").
+		Where(squirrel.Eq{"id": id}).
+		Prefix("RETURNING id, name, weight, is_hungry").
+		ToSql()
+
+	if err != nil {
+		return nil, err
+	}
+
+	row := r.Pool.QueryRow(ctx, sql)
+
+	if err := row.Scan(&response.ID, &response.Name, &response.Weight); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }
 
-func (r *AnimalRepo) GetAllAnimalsByFields(ctx context.Context, request map[string]interface{}) ([]entity.Animal, error) {
-	//implement here
-	return nil, errors.New("unimplemented method - getall animals by fields")
-}
+// func (r *AnimalRepo) GetAllAnimalsByFields(ctx context.Context, request map[string]interface{}) ([]entity.Animal, error) {
+// 	var (
+// 		response []entity.Animal
+// 	)
+
+// 	if len(request) == 0 {
+// 		r.Builder.Select()
+// 	}
+
+// 	sql, _, err := r.Builder.Select("animals").
+		
+// }
--- migrations/20210221023242_migrate_name.up.sql
+++ migrations/20210221023242_migrate_name.up.sql
@@ -1,7 +1,16 @@
-CREATE TABLE IF NOT EXISTS history(
-    id serial PRIMARY KEY,
-    source VARCHAR(255),
-    destination VARCHAR(255),
-    original VARCHAR(255),
-    translation VARCHAR(255)
+CREATE TABLE IF NOT EXISTS animals (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255),
+    weight FLOAT,
+    is_hungry BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+    id UUID PRIMARY KEY,
+    admin_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS worker (
+    id UUID PRIMARY KEY,
+    worker_name VARCHAR(255)
 );
